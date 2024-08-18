0bcef4b2bf2b35de5f3d6dac1e48af5a91186581
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-08-18 13:17:46 +0300

    configure db connection

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log("DB connected");
+        })
+
+        connection.on('error', (err) => {
+            console.log("Db connection error: " + err);
+            process.exit();
+        })
+
+    } catch (error) {
+        console.log("Something Went Wrong")
+        console.error("Error: ", error);
+
+    }
+}
