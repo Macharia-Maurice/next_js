9f8da165b975da40688a7856ee4e50c7e8fff1fc
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-08-19 19:25:12 +0300

    add  signup logic

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
+import { connect } from '@/dbConfig/dbConfig';
+import User from '@/models/UserModel';
+import { validateEmail, validatePassword } from '@/utils/validators';
+
+export async function POST(request: NextRequest) {
+    try {
+        await connect();
+
+        const { email, password, username } = await request.json();
+
+        // Check if email and password are provided
+        if (!email || !password || !username) {
+            return NextResponse.json({ success: false, message: 'Email, username and password are required' }, { status: 400 });
+        }
+
+        // Validate email format
+        if (!validateEmail(email)) {
+            return NextResponse.json({ success: false, message: 'Invalid email format' }, { status: 400 });
+        }
+
+        // Validate password strength
+        // if (!validatePassword(password)) {
+        //     return NextResponse.json({ success: false, message: 'Password must be at least 8 characters, include an uppercase letter, a number, and a special character' }, { status: 400 });
+        // }
+
+        // Check if the user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return NextResponse.json({ success: false, message: 'User with this email already exists' }, { status: 400 });
+        }
+        
+        // Hash the password
+        const hashedPassword = await bcryptjs.hash(password, 10);
+
+        // Create the new user
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        return NextResponse.json({ success: true, message: 'User registered successfully', userId: newUser._id }, { status: 201 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
+    }
+}
