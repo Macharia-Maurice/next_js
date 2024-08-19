07a2f19f1ee3eacae0ad48ca5d0e4b45471c67cb
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-08-19 19:24:37 +0300

    add login logic

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/UserModel";
+import jwt from 'jsonwebtoken';
+import { connect } from "@/dbConfig/dbConfig";
+import bcryptjs from 'bcryptjs';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
+
+export async function POST(request: NextRequest) {
+
+    try {
+        await connect();
+
+        const reqBody = await request.json();
+        const { email, password } = reqBody;
+
+        if (!email || !password) {
+            return NextResponse.json({ success: false, message: "Email and password must provided!" }, { status: 400 });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) return NextResponse.json({ success: false, message: "Invalid credentials!" }, { status: 401 });
+
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+        if (!isPasswordValid) return NextResponse.json({ success: false, message: "Invalid credentials were provided!" }, { status: 401 });
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            },
+            JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        const response = NextResponse.json({ success: true, message: "Login successful" })
+
+        response.cookies.set('token', token, {
+            httpOnly: true, // Prevents JavaScript access to the cookie
+            secure: process.env.NODE_ENV === 'production', // Ensures the cookie is only sent over HTTPS in production
+            maxAge: 60 * 60, // 1 hour in seconds
+            path: '/', // Cookie is accessible to the entire site
+            sameSite: 'strict', // Protects against CSRF attacks
+        });
+
+        return response;
+
+    } catch (error:any) {
+        console.error(error);
+        return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
+    }
+
+
+}
