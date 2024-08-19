1738f3d843ef2470399e2e00d46933e4000155a6
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-08-19 19:26:23 +0300

    regex to validate email and password

---
+++ src/utils/validators.ts
@@ -0,0 +1,10 @@
+export const validateEmail = (email: string) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+};
+
+export const validatePassword = (password: string) => {
+    // Password must be at least 8 characters, include an uppercase letter, a number, and a special character
+    const re = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return re.test(password);
+};
