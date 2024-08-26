c6a03f8a71893b40eaf6469f897ab38d2785be79
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-08-26 10:55:31 +0300

    update name of userId in response header

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -4,26 +4,25 @@ import { connect } from "@/dbConfig/dbConfig";
 
 export async function GET(request: NextRequest) {
     try {
-
         // Extract userId from headers set by the middleware
         const userId = request.headers.get('userId') || '';
 
+        if (!userId) {
+            return NextResponse.json({ success: false, message: "User ID not provided" }, { status: 400 });
+        }
+
         await connect();
 
-        const user = await User.findOne({ _id: userId }).
-            select("-password");
+        const user = await User.findOne({ _id: userId }).select("-password");
 
         if (!user) {
             return NextResponse.json({ success: false, message: "User not found" }, { status: 404 });
         }
 
-        return NextResponse.json({ success: true, message: "user found", user });
-
-
+        return NextResponse.json({ success: true, message: "User found", user });
 
     } catch (error) {
         console.error("Error fetching user info:", error);
         return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
     }
-
 }
