263febaaa6d7086c19c1cc4c990835c640edfc20
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-08-26 10:53:36 +0300

    use jose to sign jwt

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,20 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/UserModel";
-import jwt from 'jsonwebtoken';
+import { SignJWT } from 'jose';
 import { connect } from "@/dbConfig/dbConfig";
 import bcryptjs from 'bcryptjs';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
 
 export async function POST(request: NextRequest) {
-
     try {
         await connect();
 
         const { email, password } = await request.json();
 
         if (!email || !password) {
-            return NextResponse.json({ success: false, message: "Email and password must provided!" }, { status: 400 });
+            return NextResponse.json({ success: false, message: "Email and password must be provided!" }, { status: 400 });
         }
 
         const user = await User.findOne({ email });
@@ -22,17 +21,18 @@ export async function POST(request: NextRequest) {
         if (!user) return NextResponse.json({ success: false, message: "Invalid credentials!" }, { status: 401 });
 
         const isPasswordValid = await bcryptjs.compare(password, user.password);
-        if (!isPasswordValid) return NextResponse.json({ success: false, message: "Invalid credentials were provided!" }, { status: 401 });
+        if (!isPasswordValid) return NextResponse.json({ success: false, message: "Invalid credentials!" }, { status: 401 });
 
-        const token = jwt.sign(
-            {
-                id: user._id,
-            },
-            JWT_SECRET,
-            { expiresIn: '1h' }
-        );
+        const encoder = new TextEncoder();
+        const jwtSecret = encoder.encode(JWT_SECRET);
 
-        const response = NextResponse.json({ success: true, message: "Login successful" })
+        // Create JWT
+        const token = await new SignJWT({ id: user._id })
+            .setProtectedHeader({ alg: 'HS256' }) // Set algorithm in the header
+            .setExpirationTime('1h')
+            .sign(jwtSecret);
+
+        const response = NextResponse.json({ success: true, message: "Login successful" });
 
         response.cookies.set('token', token, {
             httpOnly: true, // Prevents JavaScript access to the cookie
@@ -44,10 +44,8 @@ export async function POST(request: NextRequest) {
 
         return response;
 
-    } catch (error:any) {
-        console.error("login failed Error: ",error.message);
+    } catch (error: any) {
+        console.error("Login failed with Error: ", error.message);
         return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
     }
-
-
 }
